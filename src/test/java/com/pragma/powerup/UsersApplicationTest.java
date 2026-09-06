54ac8422bea2166156d95423d67e7bc0d571b428
Matilde Alexandra Arévalo León <m.arevalo@nelumbo.com.co>
2026-09-06 00:11:27 -0500

    chore: initial project

---
+++ src/test/java/com/pragma/powerup/UsersApplicationTest.java
@@ -0,0 +1,4 @@
+package com.pragma.powerup;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+class UsersApplicationTest { @Test void applicationClassIsAvailable() { assertThat(UsersApplication.class).isNotNull(); } }
